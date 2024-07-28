20e70dba7f83c7305a2c693b17ab178f5fbcf2f6
Saraththecoder <sarathkumar86518@gmail.com>
2024-07-28 22:19:16 +0530

    Add files via upload

---
+++ dice_simulator.py
@@ -0,0 +1,4 @@
+import random
+dice=random.randint(1,6)
+print("...Welcome to Dice simulator...")
+print(f"   The Dice value is: {dice}   ")
---
+++ toss_predictor.py
@@ -0,0 +1,6 @@
+import random
+
+select=["Heads","Tails"]
+toss=random.choice(select)
+print("..Welcome to Toss Predictor...")
+print(f"    The Result is: {toss}   ")
